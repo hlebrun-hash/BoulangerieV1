8c460730d3e256dba9eea28423a8a01af9280f38
hlebrun-hash <hlebrun@eugeniaschool.com>
2026-01-04 16:36:13 +0100

    resou le problem

--- simple_server.js
+++ simple_server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const port = 8091;
+const port = 8092;
 const mimeTypes = {
     '.html': 'text/html',
     '.js': 'text/javascript',
